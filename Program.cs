2fb2892d76d14e7672c89c2f508a1e7b635ddfe7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-22 20:48:10 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IProyectRepository, ProyectRepository>();
+builder.Services.AddScoped<ProyectRepository>();
+
 
 var app = builder.Build();
 
