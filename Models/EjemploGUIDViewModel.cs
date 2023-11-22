46fc8c7ac0730b75719190794e5032d5f21b33f2
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-22 20:47:51 -0300

    Create EjemploGUIDViewModel.cs

---
+++ Models/EjemploGUIDViewModel.cs
@@ -0,0 +1,10 @@
+namespace portafolio.Servicios
+{
+    public class EjemploGUIDViewModel
+    {
+        public Guid Transitorio { get; set; }
+        public Guid Delimitado { get; set; }
+        public Guid Unico { get; set; }
+
+    }
+}
